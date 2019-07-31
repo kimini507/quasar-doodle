bf8dc2b484a6c826d4ca6594f0c3636fadddda4d
Carmela Jane Garcia <carmelajanegarcia@gmail.com>
2019-07-31 23:06:07 +0800

    Adding all testing related files

---
+++ src/components/ComponentToTest.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <q-input
+      v-model="search"
+      @input="emitSearch"
+      :debounce="debounce"
+      placeholder="Search"
+    >
+    </q-input>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    search: String,
+    debounce: {
+      default: 300
+    }
+  },
+
+  methods: {
+    emitSearch () {
+      this.$emit('search', this.search)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ test/jest/__tests__/components/ComponentToTest.spec.js
@@ -0,0 +1,51 @@
+/* eslint-disable */
+/**
+ * @jest-environment jsdom
+ */
+
+import { mount, createLocalVue, shallowMount } from '@vue/test-utils'
+import SearchComponent from 'src/components/ComponentToTest'
+import * as All from 'quasar'
+const { Quasar, date } = All
+
+const components = Object.keys(All).reduce((object, key) => {
+  const val = All[key]
+  if (val && val.component && val.component.name != null) {
+    object[key] = val
+  }
+  return object
+}, {})
+
+describe('Sample Testing', () => {
+  const localVue = createLocalVue()
+  localVue.use(Quasar, { components }) // , lang: langEn
+
+  let searchModel = 'a'
+
+  const wrapper = mount(SearchComponent, {
+    localVue,
+    propsData: {
+      search: searchModel
+    }
+  })
+
+  it('passes the sanity check and creates a wrapper', () => {
+    expect(wrapper.isVueInstance()).toBe(true)
+  })
+
+  const input = wrapper.find('input')
+
+  it('sets the placeholder as "Search"', () => {
+    expect(input.attributes('placeholder')).toBe('Search')
+  })
+
+  it('sets the search value as the passed prop', () => {
+    expect(wrapper.vm.search).toBe('2')
+  })
+
+  it('#emitSearch', () => {
+    wrapper.vm.emitSearch()
+
+    expect(wrapper.emitted('search')).toBeTruthy()
+  })
+})
